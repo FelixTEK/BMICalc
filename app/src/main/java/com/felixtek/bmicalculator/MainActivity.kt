31e87b0dcb9f3b4da51f12ff1b56c277c27fb5ed
FelixTEK <zuhong2005@gmail.com>
2024-05-25 18:24:01 +0700

    initial

---
+++ app/src/main/java/com/felixtek/bmicalculator/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.felixtek.bmicalculator
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.NumberFormatException
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btnCalc : Button
+    lateinit var etHeight : EditText
+    lateinit var etWeight : EditText
+    lateinit var displayTv : TextView
+    lateinit var unitSwitch : ToggleButton
+    var imperial = false
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnCalc = findViewById(R.id.btn_calc)
+        etHeight = findViewById(R.id.et_height)
+        etWeight = findViewById(R.id.et_weight)
+        displayTv = findViewById(R.id.display_tv)
+        unitSwitch = findViewById(R.id.unitswitch_tb)
+
+        btnCalc.setOnClickListener(this)
+
+    }
+    override fun onClick(v: View?) {
+        try {
+            var height = etHeight.text.toString().toDouble()
+            var weight = etWeight.text.toString().toDouble()
+            var result = 0.0
+            when (v?.id) {
+                R.id.btn_calc -> {
+                    unitSwitch.setOnCheckedChangeListener { _, isChecked ->
+
+                        if (isChecked) {
+                            imperial = true
+                        } else {
+                            imperial = false
+                        }
+                    }
+                    if (imperial == true) {
+                        result = (weight / (height * height)) * 703
+                    } else {
+                        result = weight / (height * height)
+                    }
+                    val formattedresult = String.format("%.2f",result)
+                    displayTv.text = "$formattedresult"
+                }
+
+            }
+        }
+
+        catch(e: NumberFormatException){
+            val message = Toast.makeText(this, "Please insert height and/or weight",Toast.LENGTH_LONG)
+            message.show()
+        }
+
+    }
+
+}
